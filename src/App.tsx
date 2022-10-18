3518bba0d16f4273522347de52661681e7628fad
Dmitriy24S <devilins24@gmail.com>
2022-10-18 12:09:23 +0300

    feat: init project - frontend vercel test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import Header from './components/Header/Header'
+import FullPost from './pages/FullPost/FullPost'
+import Home from './pages/Home/Home'
 
-function App() {
+import { Route, Routes } from 'react-router-dom'
+import CreatePost from './pages/CreatePost/CreatePost'
+import { fetchAuthMe } from './redux/slices/authSlice'
+import { useAppDispatch } from './redux/store'
+
+const App = () => {
+  const dispatch = useAppDispatch()
+
+  // const isAuth = useSelector(checkIsAuth)
+
+  // Check if logged in -> if have token in localStorage -> means logged in
+  useEffect(() => {
+    dispatch(fetchAuthMe())
+    // {success: true,…}
+    // avatarUrl:"https://www.resetera.com/forums/etcetera-forum.9/z"
+    // createdAt:"2022-10-03T17:52:04.346Z"
+    // email:"johndoe@test.com"
+    // fullName:"John Doe"
+    // success:true
+    // updatedAt:"2022-10-03T17:52:04.346Z"
+    // user:{_id: "633b2144ad263568ee0fff69", fullName: "John Doe", email: "johndoe@test.com",…}
+    // __v:0
+    // _id:"633b2144ad263568ee0fff69"
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <div className='wrapper max-w-6xl mx-auto px-4 pb-20'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/posts/:id' element={<FullPost />} />
+          <Route path='/posts/:id/edit' element={<CreatePost />} />
+          <Route path='/posts/create' element={<CreatePost />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
