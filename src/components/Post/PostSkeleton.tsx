9fa88a2e99e85dc66f5246ddba42f3c3a5cce611
Dmitriy24S <devilins24@gmail.com>
2022-10-20 16:02:11 +0300

    fix: style: postSkeleton overflow in mobile view
    text skeleton overflow in mobile view

--- src/components/Post/PostSkeleton.tsx
+++ src/components/Post/PostSkeleton.tsx
@@ -6,22 +6,23 @@ const PostSkeleton = ({ isFullPost }: { isFullPost: boolean }) => {
       {/* post image */}
       <div
         // className={clsx(styles.image, { [styles.imageFull]: isFullPost })}
-        className='image w-full h-full min-h-[275px] max-h-[324px] object-cover bg-slate-300'
+        className='image w-full h-full min-h-[190px] sm:min-h-[275px] max-h-[324px] object-cover bg-slate-300'
       />
       {/* post user & text */}
       <div className='post-content p-6 flex items-start'>
-        <div className='avatar bg-slate-300  rounded-full min-h-[2rem] min-w-[2rem] mt-1.5 mr-3.5'></div>
+        <div className='avatar bg-slate-300 rounded-full min-h-[2rem] min-w-[2rem] mt-1.5 mr-3.5'></div>
         <div className='container flex flex-col gap-7 w-full'>
           <div className='blog-top mb-1'>
             <div className='name bg-slate-300 h-5 w-32 mb-2 rounded'></div>
             <div className='time bg-slate-300 h-5 w-40 rounded'></div>
           </div>
-          <div className='title h-8 w-72 bg-slate-300 rounded'></div>
+          {/* <div className='title h-8 w-72 bg-slate-300 rounded'></div> */}
+          <div className='title h-8 w-[80%] bg-slate-300 rounded'></div>
           <div className='body-text bg-slate-300 h-14 w-full rounded'></div>
           <div className='tags'>
             <div className='list flex gap-4 flex-wrap'>
               {[...Array(3)].map((_tag, idx) => (
-                <div key={idx} className='bg-slate-300 w-12 h-6 rounded'></div>
+                <div key={idx} className='bg-slate-300 w-[18%] max-w-[3rem] h-6 rounded'></div>
               ))}
             </div>
           </div>
